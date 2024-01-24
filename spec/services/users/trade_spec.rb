bb980d8a3b3c73dae5f4b47a5dabd94c3b8b0b5d
Adriel Dinelli <adriel.dinelli@gmail.com>
2024-01-24 18:52:37 -0300

    Remove commented block from spec

--- spec/services/users/trade_spec.rb
+++ spec/services/users/trade_spec.rb
@@ -1,17 +1,6 @@
 require 'rails_helper'
 
 RSpec.context 'Services' do
-  # describe Users::Trade do
-  #   let(:user_a) { create(:user, :with_item_x10) }
-  #   let(:user_b) { create(:user, :with_item_x5) }
-
-  #   it 'execute trades between two users' do
-  #     expect{ described_class.new(offer_a, offer_b).perform }
-  #       .to change{ user_a.reload.inventory.first.item.id }.from(item_a.id).to(item_b.id)
-  #       .and change{ user_b.reload.inventory.first.id }.from(item_b.id).to(item_a.id)
-  #   end
-  # end
-
   describe Users::Trade do
     let(:user_a) { create(:user) }
     let(:user_b) { create(:user) }
